ce95bfd97c8cf7c9202510b0413dff7afa96572e
evelynmatilda <evelyn.palsson@gmail.com>
2022-02-25 13:44:12 +0100

    Filled out my database

---
+++ database.js
@@ -0,0 +1,74 @@
+"use strict";
+
+let MCUdatabase = [
+    {
+        Id: 1,
+        FilmName: "Iron Man",
+        ReleaseYear: 2008,
+        Director: "Jon Favreau",
+        Phase: 1
+    },
+    {
+        Id: 2,
+        FilmName: "Captain America: The first avenger",
+        ReleaseYear: 2011,
+        Director: "Joe Johnston",
+        Phase: 1
+    },
+    {
+        Id: 3,
+        FilmName: "Marvel's The Avengers",
+        ReleaseYear: 2012,
+        Director: "Joss Whedon",
+        Phase: 1
+    },
+    {
+        Id: 4,
+        FilmName: "Black Panther",
+        ReleaseYear: 2018,
+        Director: "Ryan Coogler",
+        Phase: 3
+    },
+    {
+        Id: 5,
+        FilmName: "Ant-Man",
+        ReleaseYear: 2015,
+        Director: "Peyton Reed",
+        Phase: 2
+    },
+    {
+        Id: 6,
+        FilmName: "Thor: Ragnarok",
+        ReleaseYear: 2017,
+        Director: "Taika Waititi",
+        Phase: 3 
+    },
+    {
+        Id: 7,
+        FilmName: "Guardians of the Galaxy",
+        ReleaseYear: 2014,
+        Director: "James Gunn",
+        Phase: 2
+    },
+    {
+        Id: 8,
+        FilmName: "Doctor Strange",
+        ReleaseYear: 2016,
+        Director: "Scott Derrickson",
+        Phase: 3
+    },
+    {
+        Id: 9,
+        FilmName: "Black Widow",
+        ReleaseYear: 2021,
+        Director: "Cate Shortland",
+        Phase: 4
+    },
+    {
+        Id: 10,
+        FilmName: "Spider-Man: No way home",
+        ReleaseYear: 2021,
+        Director: "Jon Watts",
+        Phase: 4
+    }
+]
